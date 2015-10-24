2e54ba3b77c1fc234bc8b3f2f82e17b3e9b5ea07
Zach Laine <whatwasthataddress@gmail.com>
2015-10-24 13:32:09 -0500

    Another test.

---
+++ gcc/testsuite/g++.dg/dynamic-concepts/trailing_type_specifier.C
@@ -0,0 +1,21 @@
+// { dg-options "-std=c++1z" }
+
+template <typename T>
+concept bool C () {
+  return requires (T t) {
+    {t[0]} -> double;
+  };
+}
+
+any C;
+
+any C function_1 (any C c)
+{}
+
+auto function_2 (any C c) -> const ... -> * any C
+{}
+
+auto function_3 (any C c)
+{
+  return c;
+}
